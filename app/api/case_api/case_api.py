517599afb955615a45b456772c4de4b91840d188
yangyuexiong <z87254091>
2021-08-12 21:59:44 +0800

    CaseBindDataApi

--- app/api/case_api/case_api.py
+++ app/api/case_api/case_api.py
@@ -262,26 +262,68 @@ class CaseBindDataApi(MethodView):
     """
 
     def post(self):
-        """用例配置数据"""
+        """用例配置数据post"""
 
         data = request.get_json()
         case_id = data.get('case_id')
-        var_ids = data.get('var_ids', [])
+        data_id = data.get('data_id')
+        ex_type = data.get('ex_type')
 
-        if not isinstance(var_ids, list) or not var_ids:
-            return api_result()
+        ex_type_dict = {
+            "bind": 1,
+            "untie": 2
+        }
 
         query_case = TestCase.query.get(case_id)
+        query_case_data = TestCaseData.query.get(data_id)
 
         if not query_case:
             return api_result(code=400, message='用例id不存在:{}'.format(case_id))
 
-        query_var_list = TestVariable.query.filter(TestVariable.id.in_(var_ids)).all()
+        if not query_case_data:
+            return api_result(code=400, message='用例参数id不存在:{}'.format(data_id))
 
-        query_var_ids = [v.to_json().get('id') for v in query_var_list]
+        query_bind = TestCaseDataAssBind.query.filter_by(case_id=case_id, data_id=data_id).first()
 
-        set_result = list(set(var_ids).difference(set(query_var_ids)))
-        if set_result:
-            return api_result(code=400, message='变量不存在:{}'.format(set_result))
+        if not query_bind:
+            new_bind = TestCaseDataAssBind(
+                case_id=case_id,
+                data_id=data_id,
+                creator='调试',
+                creator_id=1
+            )
+            db.session.add(new_bind)
+            db.session.commit()
+            return api_result(code=203, message='绑定成功')
 
-        # Todo 入库
+        if query_bind.is_deleted == 0:
+            return api_result(code=400, message='用例:{} 已经绑定:{}'.format(case_id, data_id))
+
+        if query_bind.is_deleted != 0:
+            query_bind.is_deleted = 0
+            db.session.commit()
+            return api_result(code=203, message='状态更新成功,绑定成功')
+
+        # if ex_type_dict.get(ex_type) == 2:
+        #     query_bind.is_deleted = query_bind.id
+        #     db.session.commit()
+        #     return api_result(code=203, message='状态更新成功,接触绑定成功')
+
+    def put(self):
+        """用例配置数据put"""
+
+        data = request.get_json()
+        case_id = data.get('case_id')
+        data_id = data.get('data_id')
+
+        query_bind = TestCaseDataAssBind.query.filter_by(case_id=case_id, data_id=data_id).first()
+
+        if query_bind:
+            query_bind.ass_resp_id_list = ass_resp_id_list
+            query_bind.ass_field_id_list = ass_field_id_list
+            query_bind.modifier = "调试"
+            query_bind.modifier_id = 1
+            db.session.commit()
+            return api_result(code=203, message='编辑成功')
+        else:
+            return api_result(code=400, message='查询为空')
