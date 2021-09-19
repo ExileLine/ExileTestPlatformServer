5c50a3300e8bfe65c8d595a00887a8554216b0af
yangyuexiong <yang6333yyx@126.com>
2021-09-19 15:09:14 +0800

    feat:Api鉴权

--- common/interceptors/ApiHook.py
+++ common/interceptors/ApiHook.py
@@ -5,17 +5,32 @@
 # @File    : ApiHook.py
 # @Software: PyCharm
 
+from loguru import logger
 from flask import request, g
 
 from app.api import api
+from app.models.admin.models import Admin
 from common.libs.public_func import print_logs
+from common.libs.auth import check_user
+from common.libs.customException import ab_code_2
 
 
 @api.before_request
 def before_request_api():
-    print('=== api_before_request ===')
+    logger.info('api_before_request')
     print_logs()
     if '/api' in request.path:
-        print('访问api')
-        g.app_user = None
-        return
+        is_token = request.headers.get('Token', None)  # 是否存在token
+        logger.info('headers是否存在key:token -> {}'.format(is_token))
+
+        # TODO 开发阶段使用的万能鉴权
+        if is_token == 'yangyuexiong':
+            g.app_user = Admin.query.get(1)
+            return
+
+        if not is_token:
+            ab_code_2(666)
+
+        token = request.headers.get('token', '')  # 提取token
+        logger.info('{}'.format(token))
+        check_user(token=token, model=Admin)  # 通过 token 查找 user,将 user 存放在全局 g 对象中
