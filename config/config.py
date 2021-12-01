9ebdb3018a07847140be98aed3da2bc9253e1ae3
yangyuexiong <yang6333yyx@126.com>
2021-12-01 10:51:16 +0800

    feat:废除旧配置注册

--- config/config.py
+++ config/config.py
@@ -39,42 +39,6 @@ def get_config():
     return conf
 
 
-def app_conf():
-    """
-    # 设置环境
-    export FLASK_ENV=development
-    export FLASK_ENV=production
-
-    PS:
-    * 由于使用PyCharm直接运行时无法通过os.environ.get('FLASK_ENV')获取到系统变量,所以export FLASK_ENV=='环境'之后FLASK_ENV依然为None。
-    ** 在Flask中FLASK_ENV==None 会默认使用production作为环境。
-    *** 需要使用终端python run.py执行。os.environ.get('FLASK_ENV')才会生效获取到设置的环境。
-    **** 为了方便使用PyCharm进行开发调试:添加使用以下代码将production覆盖。
-    解决方法:
-    (1)使用以下代码覆盖 //部署生产环境时注释以下代码(不建议使用)
-        if not os.environ.get('FLASK_ENV'):
-            config_key = 'default'
-            print('Pycharm开发环境:%s' % config_key)
-            return config_key
-    (2)在PyCharm设置变量FLASK_ENV=development
-    """
-    config_key = 'development'
-
-    if os.environ.get('FLASK_ENV') == 'development':
-        config_key = 'development'
-        # print('开发环境:%s' % config_key)
-        return config_key
-
-    elif os.environ.get('FLASK_ENV') == 'production':
-        config_key = 'production'
-        # print('生产环境:%s' % config_key)
-        return config_key
-    else:
-        config_key = 'production'
-        # print('生产环境:%s' % config_key)
-        return config_key
-
-
 class BaseConfig:
     """配置基类"""
     # SECRET_KEY = os.urandom(24)
@@ -139,8 +103,6 @@ config_obj = {
 
 if __name__ == '__main__':
     print(config_obj['default'].DB_URI)
-    cof = app_conf()
-    print(cof)
 
     print(config_obj['default'].DB_URI)
     print(config_obj['new'].DB_URI)
