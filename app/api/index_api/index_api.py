3df37ff599dc0bdc503fc801b335adc50e923b86
yangyuexiong <yang6333yyx@126.com>
2021-12-12 15:27:52 +0800

    test:测试并发

--- app/api/index_api/index_api.py
+++ app/api/index_api/index_api.py
@@ -6,12 +6,19 @@
 # @Software: PyCharm
 
 import asyncio
+import time
+
 from sqlalchemy.sql import func
 
 from all_reference import *
 from app.models.admin.models import Admin
 from app.models.test_case.models import TestCase, TestCaseData
 from app.models.test_case_scenario.models import TestCaseScenario
+from app.models.test_logs.models import TestLogs
+
+
+def my_job():
+    R.set('scheduler', 'yyx123')
 
 
 class IndexApi(MethodView):
@@ -46,4 +53,17 @@ class IndexApi(MethodView):
     async def post(self):
         data = request.get_json()
         d = data
+
+        # from ExtendRegister.apscheduler_register import scheduler
+        # scheduler.add_job(func=my_job, id='1', trigger='interval', seconds=1, replace_existing=True)
         return api_result(code=200, message='index', data=d)
+
+    def put(self):
+        """3"""
+        tl = TestLogs(
+            log_type='index',
+            creator=g.app_user.username,
+            creator_id=g.app_user.id
+        )
+        tl.save()
+        return api_result(code=203, message='index')
