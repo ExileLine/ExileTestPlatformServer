f7408f2fdc03e9fc56376a886fc11ac2e6ffe466
yangyuexiong <yang6333yyx@126.com>
2021-12-20 22:39:38 +0800

    docs:docs

--- ApplicationExample.py
+++ ApplicationExample.py
@@ -15,6 +15,7 @@ from ExtendRegister.command_register import register_commands  # commands
 from ExtendRegister.bp_register import register_bp  # 蓝图
 from ExtendRegister.conf_register import register_config  # 配置
 from ExtendRegister.db_register import db  # db
+from ExtendRegister.apscheduler_register import scheduler  # scheduler
 from ExtendRegister.model_register import *  # models
 
 
@@ -29,5 +30,7 @@ def create_app():
     register_config(app)  # 配置注册
     register_bp(app)  # 蓝图注册
     db.init_app(app)  # db注册
+    # scheduler.init_app(app)  # 定时任务
+    # scheduler.start()
     Migrate(app, db)  # ORM迁移
     return app
