eb4b233a398acbcc9db2f0787a79f330137e89ee
yangyuexiong <yang6333yyx@126.com>
2022-01-15 13:57:03 +0800

    feat:新增-DingDingApi;

--- app/api/dingding_api/dingding_api.py
+++ app/api/dingding_api/dingding_api.py
@@ -7,6 +7,7 @@
 
 
 from all_reference import *
+from app.models.push_reminder.models import DingDingConfModel
 
 
 class DingDingApi(MethodView):
@@ -24,15 +25,81 @@ class DingDingApi(MethodView):
 
     def post(self):
         """钉钉 push 新增"""
-        return
+
+        data = request.get_json()
+        title = data.get('title')
+        ding_talk_url = data.get('ding_talk_url')
+        at_mobiles = data.get('at_mobiles', [])
+        at_user_ids = data.get('at_user_ids', [])
+        is_at_all = data.get('at_user_ids', False)
+
+        if not ding_talk_url:
+            return api_result(code=400, message='ding_talk_url 不能为空')
+
+        query_dd = DingDingConfModel.query.filter_by(ding_talk_url=ding_talk_url).first()
+
+        if query_dd:
+            return api_result(code=400, message='ding_talk_url 已存在')
+
+        new_dd = DingDingConfModel(
+            title=title,
+            ding_talk_url=ding_talk_url,
+            at_mobiles=at_mobiles,
+            at_user_ids=at_user_ids,
+            is_at_all=bool(is_at_all),
+            creator=g.app_user.username,
+            creator_id=g.app_user.id
+        )
+        new_dd.save()
+
+        return api_result(code=201, message='操作成功')
 
     def put(self):
         """钉钉 push 编辑"""
-        return
+
+        data = request.get_json()
+        dd_conf_id = data.get('dd_conf_id')
+        title = data.get('title')
+        ding_talk_url = data.get('ding_talk_url')
+        at_mobiles = data.get('at_mobiles', [])
+        at_user_ids = data.get('at_user_ids', [])
+        is_at_all = data.get('at_user_ids', False)
+
+        query_dd = DingDingConfModel.query.get(dd_conf_id)
+
+        if not query_dd:
+            return api_result(code=400, message='钉钉配置id:{}数据不存在'.format(dd_conf_id))
+
+        if query_dd.ding_talk_url != ding_talk_url:
+            if DingDingConfModel.query.filter_by(ding_talk_url=ding_talk_url).all():
+                return api_result(code=400, message='ding_talk_url:{} 已经存在'.format(ding_talk_url))
+
+        query_dd.title = title
+        query_dd.ding_talk_url = ding_talk_url
+        query_dd.at_mobiles = at_mobiles
+        query_dd.at_user_ids = at_user_ids
+        query_dd.is_at_all = bool(is_at_all)
+        query_dd.modifier = g.app_user.username
+        query_dd.modifier_id = g.app_user.id
+        db.session.commit()
+
+        return api_result(code=203, message='编辑成功')
 
     def delete(self):
         """钉钉 push 删除"""
-        return
+
+        data = request.get_json()
+        dd_conf_id = data.get('dd_conf_id')
+
+        query_dd = DingDingConfModel.query.get(dd_conf_id)
+
+        if not query_dd:
+            return api_result(code=400, message='钉钉配置id:{}数据不存在'.format(dd_conf_id))
+
+        query_dd.modifier = g.app_user.username
+        query_dd.modifier_id = g.app_user.id
+        query_dd.delete()
+        return api_result(code=204, message='删除成功')
 
 
 class DingDingPushPageApi(MethodView):
@@ -43,4 +110,32 @@ class DingDingPushPageApi(MethodView):
 
     def post(self):
         """钉钉 push 分页模糊查询"""
-        return
+
+        data = request.get_json()
+        dd_conf_id = data.get('dd_conf_id')
+        title = data.get('title')
+        ding_talk_url = data.get('ding_talk_url')
+        page = data.get('page')
+        size = data.get('size')
+
+        sql = """
+        SELECT * 
+        FROM exile_ding_ding_conf  
+        WHERE 
+        id LIKE"%%" 
+        and title LIKE"%B1%" 
+        and ding_talk_url LIKE"%B1%" 
+        and is_deleted=0
+        ORDER BY create_timestamp LIMIT 0,20;
+        """
+
+        result_data = general_query(
+            model=DingDingConfModel,
+            field_list=['id', 'title', 'ding_talk_url'],
+            query_list=[dd_conf_id, title, ding_talk_url],
+            is_deleted=False,
+            page=page,
+            size=size
+        )
+
+        return api_result(code=200, message='操作成功', data=result_data)
