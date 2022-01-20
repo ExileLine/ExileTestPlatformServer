40c5c9e1827ef91d1da38f63bbd7dec127d3062a
yangyuexiong <yang6333yyx@126.com>
2022-01-20 09:52:33 +0800

    docs:docker shell

--- server_start.sh
+++ server_start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+cd /srv/ExileTestPlatformServer
+
+ExilePlatformUUID=`docker ps | grep exile_platform | awk '{print $1}'`;
+
+if [ $ExilePlatformUUID ]; then
+  docker stop $ExilePlatformUUID;
+  echo "stop success";
+fi
+
 echo y | docker system prune
-
 docker build -t 'exile_platform' .
-
-# docker run -d -p 5000:5000 --name exile_platform exile_platform
-
+echo "build success"
 docker run -d --network host --name exile_platform exile_platform
+# docker run -d -p 5000:5000 --name exile_platform exile_platform
+echo "run success"
