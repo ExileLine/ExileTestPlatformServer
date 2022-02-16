b381edc03b81148f5bd273bd24cec0a12fdde815
yangyuexiong <yang6333yyx@126.com>
2022-02-16 09:51:45 +0800

    fix:注释修正

--- app/models/admin/models.py
+++ app/models/admin/models.py
@@ -2,7 +2,7 @@
 # @Time    : 2021/5/11 下午4:32
 # @Author  : ShaHeTop-Almighty-ares
 # @Email   : yang6333yyx@126.com
-# @File    : manage.py
+# @File    : models.py
 # @Software: PyCharm
 
 from werkzeug.security import generate_password_hash, check_password_hash
