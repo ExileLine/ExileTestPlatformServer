1f126de664ee0aab7b5b8b29c5c2f755edef090f
yangyuexiong <yang6333yyx@126.com>
2022-02-22 14:34:23 +0800

    test:db test

--- common/libs/db.py
+++ common/libs/db.py
@@ -243,7 +243,7 @@ project_db = MyPyMysql(**DB, debug=CONFIG_OBJ.DEBUG)  # MySql实例
 if __name__ == '__main__':
     # 测试 MySql
     print('\n===test MySql===')
-    sql = "SELECT id,case_name FROM exile_test_case WHERE id=1;"
+    sql = "SELECT id, case_name FROM exile_test_case WHERE id=1;"
     print('ping:', project_db.db_obj().open)
     result = project_db.select(sql, only=True)
     print(result)
@@ -251,4 +251,7 @@ if __name__ == '__main__':
     # 测试 Redis
     print('\n===test Redis===')
     print('ping:', R.ping())
+    print(R)
     print(R.get('yangyuexiong'))
+    print(R.execute_command("get 127.0.0.1"))
+    print(type(R.execute_command("get 127.0.0.1")))
