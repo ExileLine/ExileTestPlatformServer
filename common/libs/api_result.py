65e3536a2001a555a56326f953851b43e74c4abd
yangyuexiong <yang6333yyx@126.com>
2022-02-22 18:45:21 +0800

    feat:优化api_result

--- common/libs/api_result.py
+++ common/libs/api_result.py
@@ -9,17 +9,23 @@
 from flask import jsonify
 
 
-# 返回格式
 def api_result(code=None, message=None, data=None, details=None, status=None):
+    """
+    返回格式
+    :param code:
+    :param message:
+    :param data:
+    :param details:
+    :param status:
+    :return:
+    """
     result = {
         "code": code,
         "message": message,
         "data": data,
     }
 
-    # if not result['data']:
-    #     result.pop('data')
-    #     return jsonify(result)
+    if not result.get('data'):
+        result.pop('data')
+
     return jsonify(result)
-
-
