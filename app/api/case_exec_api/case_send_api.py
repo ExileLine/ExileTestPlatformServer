e7ec165b3245632ce754e53b9602fda3dc89a706
yangyuexiong <yang6333yyx@126.com>
2022-03-26 14:23:31 +0800

    feat:CaseReqTestApi

--- app/api/case_exec_api/case_send_api.py
+++ app/api/case_exec_api/case_send_api.py
@@ -10,6 +10,51 @@ from decimal import Decimal
 from all_reference import *
 
 
+def var_conversion(before_var):
+    """变量转换参数"""
+
+    before_var_init = before_var
+
+    if isinstance(before_var_init, (list, dict)):
+        before_var = json.dumps(before_var, ensure_ascii=False)
+
+    result_list = re.findall('\\$\\{([^}]*)', before_var)
+
+    if not result_list:
+        return before_var_init
+
+    err_var_list = []
+    current_dict = {}
+    for res in result_list:
+        sql = """select id, var_name, var_value, var_type, is_active from exile_test_variable where var_name='{}';""".format(
+            res)
+        query_result = project_db.select(sql=sql, only=True)
+        if query_result:
+            var_value = query_result.get('var_value')
+            var_type = str(query_result.get('var_type'))
+            if var_type in var_func_dict.keys():  # 函数
+                new_val = var_func_dict.get(var_type)()
+                current_dict[res] = new_val
+            else:
+                current_dict[res] = json.loads(var_value)
+
+        elif var_func_dict.get(res):
+            current_dict[res] = var_func_dict.get(res)
+        else:
+            err_var_list.append(res)
+    if not current_dict:
+        return before_var_init
+
+    current_str = before_var
+    for k, v in current_dict.items():
+        old_var = "${%s}" % (k)
+        new_var = v
+        current_str = current_str.replace(old_var, new_var)
+    if isinstance(before_var_init, (list, dict)):
+        current_str = json.loads(current_str)
+    return current_str
+
+
 class CaseReqTestApi(MethodView):
     """
     test send
@@ -17,38 +62,59 @@ class CaseReqTestApi(MethodView):
 
     def post(self):
         data = request.get_json()
-        method = data.get('method')
-        base_url = data.get('base_url')
-        url = data.get('url')
-        headers = data.get('headers', {})
-        req_type = data.get('req_type')
-        body = data.get('body', {})
+        request_method = data.get('request_method')
+        base_url = data.get('request_base_url')
+        url = data.get('request_url')
+        request_headers = data.get('request_headers', {})
+        request_params = data.get('request_params', {})
+        request_body = data.get('request_body', {})
+        request_body_type = data.get('request_body_type')
 
+        req_type_dict = {
+            "1": {"data": request_body},
+            "2": {"json": request_body},
+            "3": {"data": request_body}
+        }
         send = {
             "url": base_url + url if base_url else url,
-            "headers": headers,
-            req_type: body
+            "headers": request_headers,
         }
+        req_json_data = req_type_dict.get(str(request_body_type))
 
-        if req_type not in ["params", "data", "json"]:
-            return api_result(code=400, message='req_type 应该为:{}'.format(["params", "data", "json"]))
+        method = request_method.lower()
+
+        if method == 'get':
+            send['params'] = request_params
+            __key = "params"
+        else:
+            send.update(req_json_data)
+            __key = "json" if "json" in send else "data"
+
+        send = var_conversion(send)
+
+        if not hasattr(requests, method):
+            return api_result(code=400, message=f'请求方式:{method}不存在')
+
+        start_time = time.time()
+        response = getattr(requests, method)(**send, verify=False)
+        end_time = time.time()
 
         try:
-            if not hasattr(requests, method):
-                return api_result(code=400, message=f'请求方式:{method}不存在')
-
-            start_time = time.time()
-            response = getattr(requests, method)(**send, verify=False)
-            end_time = time.time()
-
-            data = {
-                "request_headers": dict(headers),
-                "response_headers": dict(response.headers),
-                "response_content": response.json(),
-                "http_code": response.status_code,
-                "time": f"{Decimal((end_time - start_time) * 1000).quantize(Decimal('1'))}ms"
-            }
-            return api_result(code=200, message='操作成功', data=data)
-
+            response_content = response.json()
         except BaseException as e:
-            return api_result(code=400, message=f'{str(e)}')
+            response_content = {
+                "code": 500,
+                "message": response.text
+            }
+        data = {
+            "request_url": send.get('url'),
+            "request_method": request_method,
+            "request_body_type": __key,
+            "request_headers": dict(send.get('headers', {})),
+            "request_content": send.get(__key),
+            "response_headers": dict(response.headers),
+            "response_content": response_content,
+            "http_code": response.status_code,
+            "time": f"{Decimal((end_time - start_time) * 1000).quantize(Decimal('1'))}ms"
+        }
+        return api_result(code=200, message='操作成功', data=data)
