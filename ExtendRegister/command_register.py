abc4355c9056262086370b521e2bbb60d6a0f637
yangyuexiong <yang6333yyx@126.com>
2022-04-07 14:13:27 +0800

    feat:删除弃用的表模型以及引用

--- ExtendRegister/command_register.py
+++ ExtendRegister/command_register.py
@@ -23,7 +23,7 @@ from app.models.push_reminder.models import MailConfModel, DingDingConfModel
 from app.models.platform_conf.models import PlatformConfModel
 from app.models.ui_auto_file.models import UiAutoFile
 from app.models.test_project.models import TestProject, TestProjectVersion, TestVersionTask, TestModuleApp, \
-    MidProjectVersionAndCase, MidProjectVersionAndScenario, MidRelationCase, MidRelationScenario
+    MidProjectVersionAndCase, MidProjectVersionAndScenario
 from app.models.test_case_db.models import TestDatabases
 from app.models.safe_scan_conf.models import SafeScanConf
 
