77c6fb70d11d69891e29a7050540528e53d8b292
yangyuexiong <yang6333yyx@126.com>
2022-04-11 16:47:28 +0800

    feat:model注册修正

--- ExtendRegister/model_register.py
+++ ExtendRegister/model_register.py
@@ -5,16 +5,23 @@
 # @File    : model_register.py
 # @Software: PyCharm
 
+
+from app.models.admin.models import Admin
+from app.models.platform_conf.models import PlatformConfModel
+from app.models.test_project.models import (
+    TestProject, TestProjectVersion, TestVersionTask, TestModuleApp,
+    MidProjectVersionAndCase, MidProjectVersionAndScenario,
+    MidProjectAndCase, MidVersionAndCase, MidTaskAndCase, MidModuleAndCase,
+    MidProjectAndScenario, MidVersionAndScenario, MidTaskAndScenario, MidModuleAndScenario
+)
 from app.models.test_case.models import TestCase, TestCaseData
 from app.models.test_case_assert.models import TestCaseAssResponse, TestCaseAssField, TestCaseDataAssBind, \
     TestCaseBefore, TestCaseAfter
-from app.models.test_case_db.models import TestDatabases
 from app.models.test_case_scenario.models import TestCaseScenario
-from app.models.test_env.models import TestEnv
 from app.models.test_variable.models import TestVariable
 from app.models.test_logs.models import TestLogs, TestExecuteLogs
 from app.models.test_case_set.models import TestCaseSet
-from app.models.test_project.models import TestProject, TestProjectVersion, MidProjectVersionAndCase, \
-    MidProjectVersionAndScenario, TestVersionTask, TestModuleApp, MidProjectAndCase, MidVersionAndCase, MidTaskAndCase, \
-    MidModuleAndCase, MidProjectAndScenario, MidVersionAndScenario, MidTaskAndScenario, MidModuleAndScenario
+from app.models.test_env.models import TestEnv
+from app.models.test_case_db.models import TestDatabases
+from app.models.push_reminder.models import MailConfModel, DingDingConfModel
 from app.models.safe_scan_conf.models import SafeScanConf
