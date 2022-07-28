6d61290863428c7c3451971fbc14739b22d84f64
yangyuexiong <yang6333yyx@126.com>
2022-07-28 22:53:42 +0800

    feat:(5.0版本前置准备)-docs

--- test/test_send_mail.py
+++ test/test_send_mail.py
@@ -9,8 +9,8 @@ from common.tools.send_mail import SendEmail
 
 if __name__ == '__main__':
     SendEmail(
-        to_list=['yangyuexiong@haoyuntech.com'],
-        ac_list=['yangyuexiong@haoyuntech.com']
+        to_list=['yang6333yyx@126.com'],
+        ac_list=['yang6333yyx@126.com']
     ).send_attach(
         report_title="测试",
         html_file_path="/Users/yangyuexiong/Desktop/test_reports/api/Test_Report_2022-06-22_00_10_37_执行用例[登录成功9999999.html",
