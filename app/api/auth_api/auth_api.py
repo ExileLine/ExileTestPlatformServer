685bfef7bf8f39bbf895a7af950037aabb3febed
yangyuexiong <yang6333yyx@126.com>
2022-08-06 18:17:33 +0800

    feat:AuthApi优化

--- app/api/auth_api/auth_api.py
+++ app/api/auth_api/auth_api.py
@@ -21,8 +21,9 @@ class AuthApi(MethodView):
         token = request.headers.get('token', '')  # 提取token
         user = R.get(f'token:{token}')
         if not user:
-            return api_result(code=400, message=f'token失效:{token}', data={})
+            return api_result(code=NO_DATA, message=f'token失效:{token}')
 
         user_id = R.get(user)  # 通过手机号或其他字段获取用户id  // redis命令: get yyx
         user = Admin.query.get(user_id)  # 通过id查询用户->获取用户对象
-        return api_result(code=200, message='操作成功', data=user.to_json())
+        user_obj = user.to_json()
+        return api_result(code=SUCCESS, message='操作成功', data=user_obj)
