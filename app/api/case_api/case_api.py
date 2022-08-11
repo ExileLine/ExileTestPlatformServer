5474cb2f9a3b854d6f94e3b666a4a221563ef3a5
yangyuexiong <yang6333yyx@126.com>
2022-08-11 10:46:14 +0800

    feat:CasePageApi增加查询条件

--- app/api/case_api/case_api.py
+++ app/api/case_api/case_api.py
@@ -325,6 +325,7 @@ class CasePageApi(MethodView):
         module_id = data.get('module_id', 0)
         case_id = data.get('case_id')
         case_name = data.get('case_name', '')
+        request_url = data.get('request_url', '')
         creator_id = data.get('creator_id')
         is_deleted = data.get('is_deleted', False)
         case_status = data.get('case_status')
@@ -356,6 +357,7 @@ class CasePageApi(MethodView):
                 )
             AND is_deleted = 0
             AND case_name LIKE "%{case_name}%"
+            AND request_url LIKE "%{request_url}%"
             {f"AND case_status='{case_status}'" if case_status else ''}
             {f'AND creator_id={creator_id}' if creator_id else ''}
         ORDER BY
@@ -382,6 +384,7 @@ class CasePageApi(MethodView):
                 )
             AND is_deleted = 0
             AND case_name LIKE "%{case_name}%"
+            AND request_url LIKE "%{request_url}%"
             {f"AND case_status='{case_status}'" if case_status else ''}
             {f'AND creator_id={creator_id}' if creator_id else ''}
         """
