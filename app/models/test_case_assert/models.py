efd928d9349c4a06970bee9af320283f395e622e
yangyuexiong <yang6333yyx@126.com>
2022-08-15 16:12:45 +0800

    feat:CaseBindApi重构

--- app/models/test_case_assert/models.py
+++ app/models/test_case_assert/models.py
@@ -80,6 +80,7 @@ class TestCaseDataAssBind(BaseModel):
     is_base = db.Column(TINYINT(1, unsigned=True), default=0, comment='基础数据')
     ass_resp_id_list = db.Column(db.JSON, comment='resp断言规则list')
     ass_field_id_list = db.Column(db.JSON, comment='field断言规则list')
+    index = db.Column(BIGINT(20, unsigned=True), default=0, comment='排序')
     creator = db.Column(db.String(32), comment='创建人')
     creator_id = db.Column(BIGINT(20, unsigned=True), comment='创建人id')
     modifier = db.Column(db.String(32), comment='更新人')
