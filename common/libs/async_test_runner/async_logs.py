469efe71c19a420e91118069903163db221c37b9
yangyuexiong <yang6333yyx@126.com>
2022-09-08 14:47:49 +0800

    feat:日志初始化数据结构逻辑修正

--- common/libs/async_test_runner/async_logs.py
+++ common/libs/async_test_runner/async_logs.py
@@ -5,6 +5,7 @@
 # @File    : async_logs.py
 # @Software: PyCharm
 
+import asyncio
 
 case_logs_demo = [
     {
@@ -91,9 +92,10 @@ log_desc_dict = {
     "http_code": "=== HTTP响应码 ===",
     "response_headers": "=== 响应头 ===",
     "response_body": "=== 响应体 ===",
-    "request_after": "=== 后置准备 ===",
     "response_assert": "=== 响应断言汇总 ===",
-    "field_assert": "=== 字段断言汇总 ==="
+    "field_assert": "=== 字段断言汇总 ===",
+    "request_after": "=== 后置准备 ===",
+    "update_variable": "=== 更新变量 ==="
 }
 
 
@@ -138,16 +140,20 @@ class AsyncDataLogs:
                 "type": None,
                 "logs": []
             },
-            "request_after": {
-                "description": "参数后置准备",
-                "logs": []
-            },
             "response_assert": {
                 "description": "响应断言",
                 "logs": []
             },
             "field_assert": {
                 "description": "字段断言",
+                "logs": []
+            },
+            "request_after": {
+                "description": "参数后置准备",
+                "logs": []
+            },
+            "update_variable": {
+                "description": "更新变量记录",
                 "logs": []
             }
         }
@@ -201,13 +207,11 @@ class AsyncLogs:
     def __init__(self):
         self.case_logs_dict = {}  # 用例日志字典
         self.scenario_logs_dict = {}  # 场景日志字典
-        self.scenario_case_dict = {}  # 场景中的用例日志字典
 
-    async def gen_case_logs_dict(self, case_list, is_scenario=False):
+    async def gen_case_logs_dict(self, case_list):
         """
         生成用例日志字典模型
         :param case_list: 用例列表
-        :param is_scenario: 是否场景中的用例
         :return:
         """
 
@@ -221,10 +225,7 @@ class AsyncLogs:
                 "flag": None,
                 "data_dict": {}
             }
-            if is_scenario:
-                self.scenario_case_dict[case_id] = d
-            else:
-                self.case_logs_dict[case_id] = d
+            self.case_logs_dict[case_id] = d
 
     async def gen_scenario_logs_dict(self, scenario_list):
         """
@@ -232,18 +233,30 @@ class AsyncLogs:
         :param scenario_list: 场景列表
         :return:
         """
+
         for scenario in scenario_list:
             scenario_id = scenario.get('id')
             scenario_title = scenario.get('scenario_title')
             case_list = scenario.get('case_list')
-            await self.gen_case_logs_dict(case_list=case_list, is_scenario=True)
             d = {
                 "scenario_id": scenario_id,
                 "scenario_title": scenario_title,
                 "logs": [],
                 "flag": True,
-                "case_dict": self.scenario_case_dict
+                "case_dict": {}
             }
+            for case in case_list:
+                case_id = case.get('case_info').get('id')
+                case_name = case.get('case_info').get('case_name')
+                case_dict = {
+                    "case_id": case_id,
+                    "case_name": case_name,
+                    "logs": [],
+                    "flag": None,
+                    "data_dict": {}
+                }
+                d['case_dict'][case_id] = case_dict
+
             self.scenario_logs_dict[scenario_id] = d
 
     async def add_case_logs(self, case_id, logs):
@@ -322,18 +335,3 @@ class AsyncLogs:
             await self.set_scenario_flag(scenario_id=scenario_id, case_id=case_id, flag=flag)
         else:
             raise TypeError(f'错误logs_type:{logs_type}')
-
-    @staticmethod
-    async def gen_data_logs_obj(data_id, data_name):
-        """
-        生成参数日志对象
-        :param data_id: 参数id
-        :param data_name: 参数名称
-        :return:
-        """
-
-        return AsyncDataLogs(data_id, data_name)
-
-    async def main(self):
-        """main"""
-        return
