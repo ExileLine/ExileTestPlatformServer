75354c6828cd7b5d7f131a6913398a8073c670fc
yangyuexiong <yang6333yyx@126.com>
2022-10-14 15:20:06 +0800

    feat:封装MyAioMySQL

--- common/libs/async_db.py
+++ common/libs/async_db.py
@@ -6,23 +6,18 @@
 # @Software: PyCharm
 
 import asyncio
-import decimal
-
 import aiomysql
 
 from common.libs.db import result_format
 
 
-class AsyncMySQL:
-    """1"""
-
-
 class MyAioMySQL:
 
-    def __init__(self, loop=None, pool=None, conf_dict=None):
+    def __init__(self, loop=None, pool=None, conf_dict=None, debug=None):
         self.loop = loop
         self.pool = pool
         self.conf_dict = conf_dict
+        self.debug = debug
 
     async def init_pool(self):
         if self.pool:
@@ -38,58 +33,33 @@ class MyAioMySQL:
                 print('创建连接池异常:{}'.format(e))
 
     async def query(self, sql, only=None, size=None):
-        async with self.pool.acquire() as conn:
-            async with conn.cursor(aiomysql.DictCursor) as cur:
+        """
+        select
+        :param sql:
+        :param only:
+        :param size:
+        :return:
+        """
 
-                async def __func(r):
-                    if isinstance(r, list):
-                        new_list = []
-                        for i in r:
-                            new_r = {}
-                            for k, v in i.items():
-                                if isinstance(v, decimal.Decimal):
-                                    # v = float(decimal.Decimal(v).quantize(decimal.Decimal("0.0")))
-                                    v = str(v)
-                                    v = float(v)
-                                    new_r[k] = v
-                                else:
-                                    new_r[k] = v
-                            new_list.append(new_r)
-                        return new_list
-                    elif isinstance(r, dict):
-                        new_r = {}
-                        for k, v in r.items():
-                            if isinstance(v, decimal.Decimal):
-                                # v = float(decimal.Decimal(v).quantize(decimal.Decimal("0.0")))
-                                v = str(v)
-                                v = float(v)
-                                new_r[k] = v
-                            else:
-                                new_r[k] = v
-                        return new_r
-                    else:
-                        pass
-
-                try:
+        try:
+            async with self.pool.acquire() as conn:
+                async with conn.cursor(aiomysql.DictCursor) as cur:
                     await cur.execute(sql)
                     if only and not size:  # 唯一结果返回 json/dict
-                        rs = await cur.fetchone()
-                        result = await __func(rs)
-                        return result
-                    if size and not only:  # 按照需要的长度返回
-                        rs = await cur.fetchmany(size)
-                        result = await __func(rs)
-                        return result
-                    else:  # 返回结果集返回 list
-                        rs = await cur.fetchall()
-                        result = await __func(rs)
-                        return result
-
-                except BaseException as e:
-                    print('查询异常:{}'.format(e))
-                # finally:
-                # 释放掉conn,将连接放回到连接池中
-                # await self.pool.release(conn)
+                        query_result = await cur.fetchone()
+                        result = result_format(query_result)
+                    elif size and not only:  # 按照需要的长度返回
+                        query_result = await cur.fetchmany(size)
+                        result = [result_format(q) for q in query_result]
+                    else:
+                        query_result = await cur.fetchall()
+                        result = [result_format(q) for q in query_result]
+            return result
+        except BaseException as e:
+            return 'select:出现错误:{}'.format(str(e) if self.debug else '')
+        # finally:
+        # 释放掉conn,将连接放回到连接池中
+        # await self.pool.release(conn)
 
     async def execute(self, sql):
         async with self.pool.acquire() as conn:
@@ -97,7 +67,7 @@ class MyAioMySQL:
                 try:
                     await cur.execute(sql)
                 except BaseException as e:
-                    print(str(e))
+                    print('sql执行出现错误:{}'.format(str(e) if self.debug else ''))
                     await conn.rollback()
                 # else:
                 #     affected = cur.rowcount
@@ -119,13 +89,12 @@ if __name__ == '__main__':
         await db.init_pool()
 
         sql = "SELECT id, case_name FROM exile_test_case limit 0,6;"
-
-        r = await db.query(sql=sql)
-        print(r)
-        r = await db.query(sql=sql, size=1)
-        print(r)
-        r = await db.query(sql=sql, only=True)
-        print(r)
+        result1 = await db.query(sql, only=True)
+        result2 = await db.query(sql, size=3)
+        result3 = await db.query(sql)
+        print(result1, type(result1), len(result1))
+        print(result2, type(result2), len(result2))
+        print(result3, type(result3), len(result3))
 
 
     asyncio.run(main())
