17460a7c1f4e0c710a4d61adde24392bf454d6f0
yangyuexiong <yang6333yyx@126.com>
2022-12-31 19:09:42 +0800

    feat:删除IndexApi调试代码

--- app/api/index_api/index_api.py
+++ app/api/index_api/index_api.py
@@ -6,11 +6,6 @@
 # @Software: PyCharm
 
 from all_reference import *
-from app.models.admin.models import Admin
-from app.models.test_case.models import TestCase, TestCaseData
-from app.models.test_case_scenario.models import TestCaseScenario
-from app.models.test_logs.models import TestLogs
-from common.tools.clear_cache import ClearCache
 
 
 class IndexApi(MethodView):
@@ -19,51 +14,17 @@ class IndexApi(MethodView):
     """
 
     async def get(self):
-        """统计"""
-
-        total_user = Admin.query.count()
-        total_case = TestCase.query.count()
-        total_case_execute = TestCase.query.with_entities(func.sum(TestCase.total_execution)).scalar()
-        total_case_data = TestCaseData.query.count()
-        total_scenario = TestCaseScenario.query.count()
-        total_scenario_execute = TestCaseScenario.query.with_entities(
-            func.sum(TestCaseScenario.total_execution)).scalar()
-
-        data = {
-            "total_user": total_user,
-            "total_case": total_case,
-            "total_case_execute": total_case_execute,
-            "total_case_data": total_case_data,
-            "total_scenario": total_scenario,
-            "total_scenario_execute": total_scenario_execute,
-            "total_execute_success": 9999,
-            "total_execute_fail": 7777
-        }
-
-        return api_result(code=200, message='index', data=data)
+        """index GET"""
+        return api_result(code=SUCCESS, message='index GET', data=[])
 
     async def post(self):
-        """2"""
-
-        data = request.get_json()
-        return api_result(code=200, message='index', data=data)
+        """index POST"""
+        return api_result(code=POST_SUCCESS, message='index POST', data=[])
 
     async def put(self):
-        """3"""
-
-        tl = TestLogs(
-            log_type='index',
-            creator=g.app_user.username,
-            creator_id=g.app_user.id
-        )
-        tl.save()
-        return api_result(code=203, message='index')
+        """index PUT"""
+        return api_result(code=PUT_SUCCESS, message='index PUT', data=[])
 
     async def delete(self):
-        """4"""
-
-        r1 = ClearCache.clear_test_log()
-        r2 = ClearCache.clear_first_log()
-        r3 = ClearCache.clear_cicd()
-        r4 = ClearCache.clear_exile_test_logs()
-        return api_result(code=204, message='index', data=[r1, r2, r3, r4])
+        """index DELETE"""
+        return api_result(code=DEL_SUCCESS, message='index DELETE', data=[])
