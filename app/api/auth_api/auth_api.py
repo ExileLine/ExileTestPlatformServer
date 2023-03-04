769d9f1639b76c46b8a60c90bc90dc55372941f7
yangyuexiong <yang6333yyx@126.com>
2023-03-04 18:43:49 +0800

    feat:修正AuthApi鉴权验证

--- app/api/auth_api/auth_api.py
+++ app/api/auth_api/auth_api.py
@@ -6,7 +6,6 @@
 # @Software: PyCharm
 
 from all_reference import *
-from app.models.admin.models import Admin
 
 
 class AuthApi(MethodView):
@@ -18,12 +17,8 @@ class AuthApi(MethodView):
     def get(self):
         """验证token"""
 
-        token = request.headers.get('token', '')  # 提取token
-        user = R.get(f'token:{token}')
+        token = request.headers.get('token', '')
+        user = Token.get_user_info(token)
         if not user:
             return api_result(code=UNAUTHORIZED, message=f'token失效:{token}')
-
-        user_id = R.get(user)  # 通过手机号或其他字段获取用户id  // redis命令: get yyx
-        user = Admin.query.get(user_id)  # 通过id查询用户->获取用户对象
-        user_obj = user.to_json()
-        return api_result(code=SUCCESS, message=SUCCESS_MESSAGE, data=user_obj)
+        return api_result(code=SUCCESS, message=SUCCESS_MESSAGE, data=user)
