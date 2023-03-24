964ffcda859b3d2dfa699f0c6e95c73db1cecd71
yangyuexiong <yang6333yyx@126.com>
2023-03-24 11:38:54 +0800

    feat:优化邮箱配置

--- app/api/mail_api/mail_api.py
+++ app/api/mail_api/mail_api.py
@@ -29,18 +29,17 @@ class MailApi(MethodView):
         data = request.get_json()
         mail_list = data.get('mail_list', [])
         if not mail_list:
-            return api_result(code=400, message='不能为空')
+            return api_result(code=NO_DATA, message='不能为空')
 
         for mail in mail_list:
             current_mail = mail.get('mail')
             current_mail_user = mail.get('mail_user')
             if "@" not in current_mail:
-                return api_result(code=400, message=f'{current_mail}:邮箱格式错误')
+                return api_result(code=BUSINESS_ERROR, message=f'{current_mail}:邮箱格式错误')
 
-            query_mail = MailConfModel.query.filter_by(mail=current_mail).first()
-
+            query_mail = MailConfModel.query.filter_by(mail=current_mail, is_deleted=0).first()
             if query_mail:
-                return api_result(code=400, message=f'{current_mail}:已经存在')
+                return api_result(code=UNIQUE_ERROR, message=f'{current_mail}:已经存在')
 
             new_mail = MailConfModel(
                 mail=current_mail,
@@ -50,8 +49,7 @@ class MailApi(MethodView):
             )
             db.session.add(new_mail)
         db.session.commit()
-
-        return api_result(code=201, message=SUCCESS_MESSAGE)
+        return api_result(code=POST_SUCCESS, message=SUCCESS_MESSAGE)
 
     def put(self):
         """邮箱编辑"""
@@ -63,13 +61,12 @@ class MailApi(MethodView):
         is_deleted = data.get('is_deleted', False)
 
         query_mail = MailConfModel.query.get(mail_id)
-
         if not query_mail:
-            return api_result(code=400, message='邮箱id:{}数据不存在'.format(mail_id))
+            return api_result(code=NO_DATA, message=f'邮箱id: {mail_id} 不存在')
 
         if query_mail.mail != mail:
             if MailConfModel.query.filter_by(mail=mail).all():
-                return api_result(code=400, message='邮箱:{} 已经存在'.format(mail))
+                return api_result(code=UNIQUE_ERROR, message=f'邮箱: {mail} 已经存在')
 
         query_mail.mail = mail
         query_mail.mail_user = mail_user
@@ -77,24 +74,22 @@ class MailApi(MethodView):
         query_mail.modifier_id = g.app_user.id
         query_mail.is_deleted = is_deleted
         db.session.commit()
-
-        return api_result(code=203, message=PUT_MESSAGE)
+        return api_result(code=PUT_SUCCESS, message=PUT_MESSAGE)
 
     def delete(self):
         """邮箱删除"""
 
         data = request.get_json()
-        mail_id = data.get('mail_id')
+        mail_id = data.get('id')
 
         query_mail = MailConfModel.query.get(mail_id)
-
         if not query_mail:
-            return api_result(code=400, message='邮箱id:{}数据不存在'.format(mail_id))
+            return api_result(code=NO_DATA, message=f'邮箱id: {mail_id}  不存在')
 
         query_mail.modifier = g.app_user.username
         query_mail.modifier_id = g.app_user.id
         query_mail.delete()
-        return api_result(code=204, message=DEL_MESSAGE)
+        return api_result(code=DEL_SUCCESS, message=DEL_MESSAGE)
 
 
 class MailPageApi(MethodView):
@@ -110,6 +105,8 @@ class MailPageApi(MethodView):
         mail_id = data.get('mail_id')
         mail = data.get('mail')
         mail_user = data.get('mail_user')
+        is_deleted = data.get('is_deleted', 0)
+        creator_id = data.get('creator_id')
         page = data.get('page')
         size = data.get('size')
 
@@ -125,6 +122,8 @@ class MailPageApi(MethodView):
 
         where_dict = {
             "id": mail_id,
+            "is_deleted": is_deleted,
+            "creator_id": creator_id
         }
 
         result_data = general_query(
@@ -135,5 +134,4 @@ class MailPageApi(MethodView):
             page=page,
             size=size
         )
-
-        return api_result(code=200, message=SUCCESS_MESSAGE, data=result_data)
+        return api_result(code=POST_SUCCESS, message=SUCCESS_MESSAGE, data=result_data)
