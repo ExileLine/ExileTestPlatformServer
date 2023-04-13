52a4c146529a408b5e1a2f9f4b9f0bced5bbe7fc
yangyuexiong <yang6333yyx@126.com>
2023-04-13 14:50:17 +0800

    feat:优化general_query;单元测试

--- common/libs/query_related.py
+++ common/libs/query_related.py
@@ -6,6 +6,7 @@
 # @Software: PyCharm
 
 import sqlalchemy
+from sqlalchemy import or_, and_, func
 from flask_sqlalchemy.model import DefaultMeta
 
 from common.libs.set_app_context import set_app_context
@@ -14,7 +15,7 @@ from app.models.test_case_assert.models import TestCaseDataAssBind, TestCaseAsse
 from app.models.test_project.models import TestProjectVersion, MidVersionCase, MidModuleCase, TestModuleApp
 
 
-def page_size(page=None, size=None, **kwargs) -> (int, int):
+def page_size(page: int = None, size: int = None, **kwargs) -> (int, int):
     """
 
     :param page: -> int
@@ -139,33 +140,33 @@ def general_query(model: type, page: int, size: int, like_rule: str = "and_", fi
     """
     通用分页模糊查询
     :param model: -> DefaultMeta
-    :param field_list: -> list -> 模糊查询的字段列表 如: ['id','username']
-    :param query_list: -> list -> 模糊查询入参列表,对应表字段的位置 如: ['id','username'] 对应 [1,'yyx']
-    :param where_dict: -> dict -> {"id":1 ...}
-    :param like_rule: -> str -> and;or; -> like规则目前仅支持使用其中一个
-    :param in_field_list: -> list -> in的字段列表 如: ['id','username']
-    :param in_value_list: -> list -> in的入参列表 如: ['id','username'] 对应 [[1,2,3],['y1','y2','y3']] => model.id.in_([1, 2, 3])
-    :param field_order_by: -> str -> 字段名称
+    :param field_list: 模糊查询的字段列表 如: ['id','username'], 结合如下`query_list`使用
+    :param query_list: 模糊查询入参列表,对应表字段的位置 如: ['id','username'] 对应 [1,'yyx']
+    :param where_dict: where条件, {"id":1,"username":"yyx"...} 即: select ... where id=1 and username="yyx";
+    :param like_rule: like规则,目前仅支持使用其中一个 -> and_; or_;
+    :param in_field_list: in语句的字段列表 如: ['id','username'], 结合如下`in_value_list`使用
+    :param in_value_list: in语句的入参列表 如: ['id','username'] 即: [[1,2,3],['y1','y2','y3']] select ... where id in (1,2,3) and username in ('y1','y2','y3')";
+    :param field_order_by: order_by字段名称
     :param page: 页数 -> {"page":1,"size":20}
     :param size: 条数 -> {"page":1,"size":20}
     :return:
 
     """
 
-    if like_rule not in ['and_', 'or_']:
-        raise TypeError('参数错误 -> like_rule 应该是 -> and_ 或者 or_')
-
     if not isinstance(model, DefaultMeta):
         raise TypeError('类型错误 -> model 应该是 -> flask_sqlalchemy.model.DefaultMeta')
+
+    if not isinstance(page, int) or not isinstance(size, int):
+        raise TypeError('类型错误 -> page 和 size 应该是 -> int')
+
+    if like_rule not in ['and_', 'or_']:
+        raise TypeError('参数错误 -> like_rule 应该是 -> and_ 或者 or_')
 
     if not isinstance(field_list, list):
         raise TypeError('类型错误 -> field_list 应该是 -> list ')
 
     if not isinstance(query_list, list):
         raise TypeError('类型错误 -> query_list 应该是 -> list')
-
-    if not isinstance(page, int) or not isinstance(size, int):
-        raise TypeError('类型错误 -> page 和 size 应该是 -> int')
 
     in_list = []
 
@@ -189,9 +190,9 @@ def general_query(model: type, page: int, size: int, like_rule: str = "and_", fi
     where_list = [getattr(model, key) == val for key, val in where_dict.items() if val or isinstance(val, int)]
 
     result = getattr(model, 'query').filter(
-        like_rule_func(*like_list),
-        *where_list,
-        *in_list
+        like_rule_func(*like_list) if like_list else and_(True),  # 模糊查询
+        *where_list,  # where 条件
+        *in_list  # in 条件
     ).order_by(
         getattr(model, 'update_time').desc() if not field_order_by else getattr(model, field_order_by).desc()
     ).paginate(
@@ -214,20 +215,73 @@ if __name__ == '__main__':
         """测试通用分页模糊查询"""
 
         @staticmethod
-        def test_query_where():
+        def test_where():
+            """测试:where"""
+
             where_dict = {
                 "id": 1,
                 "is_deleted": 0
             }
             result_data = general_query(
                 model=TestCase,
-                field_list=['case_name'],
-                query_list=[''],
                 where_dict=where_dict,
                 page=1,
                 size=10
             )
-            print(result_data)
+            records = result_data.get('records')
+            print(records)
+
+        @staticmethod
+        def test_in():
+            """测试:in"""
+
+            result_data = general_query(
+                model=TestCase,
+                in_field_list=["id"],
+                in_value_list=[[1, 2, 3]],
+                where_dict={},
+                page=1,
+                size=10
+            )
+            records = result_data.get('records')
+            for index, res in enumerate(records, 1):
+                print(res)
+
+        @staticmethod
+        def test_like():
+            """测试:like"""
+
+            result_data = general_query(
+                model=TestCase,
+                field_list=['case_name'],
+                query_list=['okc'],
+                page=1,
+                size=100
+            )
+            records = result_data.get('records')
+            for index, res in enumerate(records, 1):
+                print(res.get('case_name'))
+
+        @staticmethod
+        def test_all():
+            """测试组合"""
+
+            where_dict = {
+                "is_deleted": 0
+            }
+            result_data = general_query(
+                model=TestCase,
+                field_list=['case_name'],
+                query_list=['okc'],
+                where_dict=where_dict,
+                in_field_list=["id"],
+                in_value_list=[[1, 2, 3, 4]],
+                page=1,
+                size=100
+            )
+            records = result_data.get('records')
+            for index, res in enumerate(records, 1):
+                print(res.get('id'), res.get('case_name'))
 
 
     @set_app_context
@@ -236,8 +290,7 @@ if __name__ == '__main__':
         MapToJsonObj.gen_bind(190)
 
 
-    # main_test()
-    # query_case_zip(case_id=14)
-    # query_case_assemble(185)
-
-    TestGQ.test_query_where()
+    # TestGQ.test_where()
+    # TestGQ.test_in()
+    # TestGQ.test_like()
+    TestGQ.test_all()
