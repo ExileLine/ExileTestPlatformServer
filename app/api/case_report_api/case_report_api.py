81d94322cccb521bb37bd7784aac7ef705bcb227
yangyuexiong <yang6333yyx@126.com>
2023-06-01 14:01:30 +0800

    feat:CaseReportApi生成测试报告上下文修正

--- app/api/case_report_api/case_report_api.py
+++ app/api/case_report_api/case_report_api.py
@@ -5,7 +5,9 @@
 # @File    : case_report_api.py
 # @Software: PyCharm
 
+
 from all_reference import *
+from config.config import config_obj
 
 demo_html = """
         <!DOCTYPE html>
@@ -23,6 +25,16 @@ demo_html = """
 """
 
 
+def save_html(ctn):
+    """保存html文件,用于测试"""
+
+    import shortuuid
+
+    fn = f'/Users/yangyuexiong/Desktop/ExileTestPlatformServer/app/templates/xxx_{shortuuid.uuid()}.html'
+    with open(fn, "w") as f:
+        f.write(ctn)
+
+
 class CaseReportApi(MethodView):
     """
     测试报告 Api
@@ -35,4 +47,15 @@ class CaseReportApi(MethodView):
         if not query_logs_json:
             return api_result(code=NO_DATA, message="日志不存在无法生成测试报告")
 
-        return render_template('test_report.html', **{"logs_json": query_logs_json})
+        debug = config_obj['new'].DEBUG
+        run_host = config_obj['new'].RUN_HOST
+        run_port = config_obj['new'].RUN_PORT
+        title = f"Exile测试报告{'Dev' if debug else ''}"
+        static_path = f"http://{run_host}:{run_port}/{'static' if debug else 'report_static'}"
+        ctx = {
+            "title": title,
+            "static_path": static_path,
+            "logs_json": query_logs_json
+        }
+        html_str = render_template('test_report.html', **ctx)
+        return html_str
